80540118fad969a0661468a9f639a18032966aeb
Lucas Liu <anyofyou@gmail.com>
2016-04-28 10:58:56 +0800

    Support threadtime format

--- ColoredLogCat.py
+++ ColoredLogCat.py
@@ -78,15 +78,42 @@ def allocate_color(tag):
     LAST_USED.append(color)
     return color
 
+def regex_match(line):
+    log_type = LOG_TYPE_UNKNOWN
+    tagtype, tag, owner, message, time = '','','','',''
+    match = retag.match(line)
+    if match:
+            log_type = LOG_TYPE_BRIEF
+            tagtype, tag, owner, message = match.groups()
+    else:
+        match = retag_threadtime.match(line)
+        if match:
+            log_type = LOG_TYPE_THREADTIME
+            time, owner, tid, tagtype, tag, message = match.groups()
+    return log_type, tagtype, tag, owner, message, time
+
+# Don't use it, we need do it one everyline.
+def set_log_type(line):
+    match = retag.match(line)
+    if match:
+        LOG_TYPE = LOG_TYPE_BRIEF
+    else:
+        match = retag_threadtime.match(line)
+        if match:
+            LOG_TYPE = LOG_TYPE_THREADTIME
+        else:
+            LOG_TYPE = LOG_TYPE_UNKNOWN
 
 RULES = {
     #re.compile(r"([\w\.@]+)=([\w\.@]+)"): r"%s\1%s=%s\2%s" % (format(fg=BLUE), format(fg=GREEN), format(fg=BLUE), format(reset=True)),
 }
 
+TIME_WIDTH = 20
 TAGTYPE_WIDTH = 3
-TAG_WIDTH = 20
+TAG_WIDTH = 25
 PROCESS_WIDTH = 8 # 8 or -1
 HEADER_SIZE = TAGTYPE_WIDTH + 1 + TAG_WIDTH + 1 + PROCESS_WIDTH + 1
+HEADER_SIZE_THREADTIME = TAGTYPE_WIDTH + 1 + TAG_WIDTH + 1 + PROCESS_WIDTH + 1 + TIME_WIDTH + 1
 
 TAGTYPES = {
     "V": "%s%s%s " % (format(fg=WHITE, bg=BLACK), "V".center(TAGTYPE_WIDTH), format(reset=True)),
@@ -96,7 +123,14 @@ TAGTYPES = {
     "E": "%s%s%s " % (format(fg=BLACK, bg=RED), "E".center(TAGTYPE_WIDTH), format(reset=True)),
 }
 
+LOG_TYPE_UNKNOWN = 0
+LOG_TYPE_BRIEF = 1
+LOG_TYPE_THREADTIME = 2
+
+LOG_TYPE = LOG_TYPE_UNKNOWN
+
 retag = re.compile("^([A-Z])/([^\(]+)\(([^\)]+)\): (.*)$")
+retag_threadtime = re.compile("^(.*\ .*)\s+(\d+)\s+(\d+)\s+([A-Z])\s+(.+): (.*)$")
 
 # to pick up -d or -e
 adb_args = ' '.join(sys.argv[1:])
@@ -113,20 +147,28 @@ while True:
     except KeyboardInterrupt:
         break
 
-    match = retag.match(line)
-    if not match is None:
-        tagtype, tag, owner, message = match.groups()
+    result = regex_match(line)
+    if result[0] != LOG_TYPE_UNKNOWN:
+        tagtype, tag, owner, message, time = result[1:]
+        # print tagtype
+        # print tag
+        # print owner
+        # print message
         linebuf = StringIO.StringIO()
 
+        if len(time) >0:
+            time = time.strip().ljust(TIME_WIDTH)
+            linebuf.write("%s%s%s" % (format(fg=CYAN, bright=True), time, format(reset=True)))
         # center process info
         if PROCESS_WIDTH > 0:
             owner = owner.strip().center(PROCESS_WIDTH)
+            # owner = owner.strip().ljust(5)
             linebuf.write("%s%s%s " % (format(fg=BLACK, bg=BLACK, bright=True), owner, format(reset=True)))
 
         # right-align tag title and allocate color if needed
         tag = tag.strip()
         color = allocate_color(tag)
-        tag = tag[-TAG_WIDTH:].rjust(TAG_WIDTH)
+        tag = tag[-TAG_WIDTH:].ljust(TAG_WIDTH)
         linebuf.write("%s%s %s" % (format(fg=color, dim=False), tag, format(reset=True)))
 
         # write out tagtype colored edge
@@ -134,8 +176,10 @@ while True:
         linebuf.write(TAGTYPES[tagtype])
 
         # insert line wrapping as needed
-        message = indent_wrap(message, HEADER_SIZE, WIDTH)
-
+        if result[0] == LOG_TYPE_BRIEF:
+            message = indent_wrap(message, HEADER_SIZE, WIDTH)
+        else:
+            message = indent_wrap(message, HEADER_SIZE_THREADTIME, WIDTH)
         # format tag message using rules
         for matcher in RULES:
             replace = RULES[matcher]
